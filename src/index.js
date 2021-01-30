1f47c162f22632ceca8352f468bfcb17da002387
xiaozi <245565986@qq.com>
2021-01-31 00:09:25 +0800

    升级包版本

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import puppeteer from 'puppeteer'
-import genericPool from 'generic-pool'
-import initDebug from 'debug'
-const debug = initDebug('puppeteer-pool')
+import puppeteer from 'puppeteer';
+import genericPool from 'generic-pool';
 
 const initPuppeteerPool = ({
   max = 10,
@@ -12,59 +10,85 @@ const initPuppeteerPool = ({
   // specifies the maximum number of times a resource can be reused before being destroyed
   maxUses = 50,
   testOnBorrow = true,
-  puppeteerArgs = [],
+  puppeteerArgs = {},
   validator = () => Promise.resolve(true),
   ...otherConfig
 } = {}) => {
   // TODO: randomly destroy old instances to avoid resource leak?
+  const cachedInstances = {};
   const factory = {
-    create: () => puppeteer.launch(...puppeteerArgs).then(instance => {
-      instance.useCount = 0
-      return instance
-    }),
-    destroy: (instance) => {
-      instance.close()
+    create: () =>
+      puppeteer.launch({ ...puppeteerArgs }).then(instance => {
+        const pid = instance.process().pid;
+        cachedInstances[pid] = {
+          useCount: 0,
+          valid: true
+        };
+        return instance;
+      }),
+    destroy: instance => {
+      const pid = instance.process().pid;
+      delete cachedInstances[pid];
+      instance.close();
     },
-    validate: (instance) => {
-      return validator(instance)
-        .then(valid => Promise.resolve(valid && (maxUses <= 0 || instance.useCount < maxUses)))
-    },
-  }
+    validate: instance => {
+      return validator(instance).then(valid => {
+        const pid = instance.process().pid;
+        const useCount = cachedInstances[pid].useCount;
+        return Promise.resolve(
+          valid &&
+            cachedInstances[pid].valid &&
+            (maxUses <= 0 || useCount < maxUses)
+        );
+      });
+    }
+  };
   const config = {
     max,
     min,
     idleTimeoutMillis,
     testOnBorrow,
-    ...otherConfig,
-  }
-  const pool = genericPool.createPool(factory, config)
-  const genericAcquire = pool.acquire.bind(pool)
-  pool.acquire = () => genericAcquire().then(instance => {
-    instance.useCount += 1
-    return instance
-  })
-  pool.use = (fn) => {
-    let resource
-    return pool.acquire()
+    ...otherConfig
+  };
+
+  const pool = genericPool.createPool(factory, config);
+  const genericAcquire = pool.acquire.bind(pool);
+  pool.acquire = () =>
+    genericAcquire().then(instance => {
+      const pid = instance.process().pid;
+      cachedInstances[pid].useCount += 1;
+      return instance;
+    });
+  pool.use = fn => {
+    let resource;
+    return pool
+      .acquire()
       .then(r => {
-        resource = r
-        return resource
+        resource = r;
+        return resource;
       })
       .then(fn)
-      .then((result) => {
-        pool.release(resource)
-        return result
-      }, (err) => {
-        pool.release(resource)
-        throw err
-      })
-  }
+      .then(
+        result => {
+          pool.release(resource);
+          return result;
+        },
+        err => {
+          pool.release(resource);
+          throw err;
+        }
+      );
+  };
+  pool.invalidate = instance => {
+    const pid = instance.process().pid;
+    cachedInstances[pid].valid = false;
+  };
 
-  return pool
-}
+  return pool;
+};
 
 // To avoid breaking backwards compatibility
 // https://github.com/binded/phantom-pool/issues/12
-initPuppeteerPool.default = initPuppeteerPool
+initPuppeteerPool.default = initPuppeteerPool;
 
-export default initPuppeteerPool
+export default initPuppeteerPool;
